b05e8ff8c9caa08182de697a9e829c783ed8c2be
Adilson Jr <adilson@ideatree.com.br>
2017-03-09 16:10:16 -0300

    Readme creation

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Browser Scripts
+===============
+
+Some useful Grease Monkey scripts for a better experience on websites originally built with no user experience in mind.
+
+You can import anyone of them pasting its content on a new user script created on the "User Scripts" section on the ```about:addons``` if you are using Firefox.
+
+- [Unlocalizer](/unlocalizer): this script allow automatic redirection from a translated content (fr-FR/pt-BR/es-ES, etc) to the original en-US.
---
+++ Unlocalizer/Unlocalizer.js
@@ -0,0 +1,34 @@
+// Encoding: UTF-8 without BOM (auto-detect: °°°°°)
+
+// ==UserScript==
+// @namespace http://adilsonjr.com/
+// @name Display websites content in English
+// @name:pt Exibe conteúdo de sites em inglês
+// @description Redirects regional websites to the en-US original site to prevent annoying translation elements to appear.
+// @description:pt Redireciona websites localizados para a versão original em ingles, para evitar traduções de má qualidade. 
+// @version 2017.1.1
+// @include http://msdn.microsoft.com/*
+// @include https://msdn.microsoft.com/*
+// @include http://azure.microsoft.com/*
+// @include https://azure.microsoft.com/*
+// @include http://docs.microsoft.com/*
+// @include https://docs.microsoft.com/*
+// @include https://developer.mozilla.org/*
+// @include http://developer.mozilla.org/*
+// @run-at document-start
+// ==/UserScript==
+
+(function() {
+	var matches = location.pathname.match(/^\/([a-z]{2}-[a-z]{2})\//i);
+	if (matches)
+	{
+		var locale = matches[1];
+		if (locale && !locale.match(/en-us/i))
+		{
+			var newUrl = location.href.replace("/" + locale + "/", "/en-us/");
+			location.replace(newUrl);
+			return;
+		}
+	}
+
+})()
